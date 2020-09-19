7205cebd5dcb6957d84092407becc9f4749d8dc9
Huynh, William <huynhwi2@msu.edu>
2020-09-19 16:27:33 -0400

    add Datafiles

---
+++ Game Sense/VideoGameData.swift
@@ -0,0 +1,37 @@
+//
+//  VideoGameData.swift
+//  Game Sense
+//
+//  Created by William Huynh on 9/19/20.
+//  Copyright © 2020 wi2. All rights reserved.
+//
+
+import Foundation
+
+class VideoGameData {
+    let spiderman = [
+        "ml_title":"Spiderman 2",
+        "title":"Marvel's Spider-Man: Miles Morales",
+        "image":"https://images.igdb.com/igdb/image/upload/t_cover_big/co2dwe.jpg",
+        "company":"Insomniac Games",
+        "description":"The latest adventure in the Spider-Man universe will build on and expand ‘Marvel’s Spider-Man’ through an all-new story. Players will experience the rise of Miles Morales as he masters new powers to become his own Spider-Man."
+    ]
+        
+    let amongUs = [
+        "ml_title":"Among Us",
+        "title":"Among Us",
+        "image":"https://images.igdb.com/igdb/image/upload/t_cover_big/co1uaf.jpg",
+        "company":"InnerSloth",
+        "description":"Play online or via local WiFi with 4-10 players as you attempt to prep your spaceship for departure, but beware as one will be an impostor bent on killing everyone!"
+    ]
+        
+    let mario = [
+        "ml_title":"Super Mario 3D",
+        "title":"Super Mario 3D World",
+        "image":"https://images.igdb.com/igdb/image/upload/t_cover_big/co21vd.jpg",
+        "company":"Nintendo",
+        "description":"The cat's out of the bag-Super Mario 3D World is coming to the Nintendo Switch system! Pounce and climb through dozens of colorful stages! Mario (and his friends) can use power-ups like the Super Bell, which grants catlike abilities, like climbing and scratching."
+    ]
+    
+    
+}
