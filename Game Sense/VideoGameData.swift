1d6a79ea7e5c538c8590d6b49f8ef616af55f701
Huynh, William <huynhwi2@msu.edu>
2020-09-19 16:42:02 -0400

    recommendations

--- Game Sense/VideoGameData.swift
+++ Game Sense/VideoGameData.swift
@@ -14,24 +14,27 @@ class VideoGameData {
         "title":"Marvel's Spider-Man: Miles Morales",
         "image":"https://images.igdb.com/igdb/image/upload/t_cover_big/co2dwe.jpg",
         "company":"Insomniac Games",
-        "description":"The latest adventure in the Spider-Man universe will build on and expand ‘Marvel’s Spider-Man’ through an all-new story. Players will experience the rise of Miles Morales as he masters new powers to become his own Spider-Man."
-    ]
+        "description":"The latest adventure in the Spider-Man universe will build on and expand ‘Marvel’s Spider-Man’ through an all-new story. Players will experience the rise of Miles Morales as he masters new powers to become his own Spider-Man.",
+        "recommendations":["https://images.igdb.com/igdb/image/upload/t_cover_big/co1r77.jpg","https://images.igdb.com/igdb/image/upload/t_cover_big/co2edq.jpg","https://images.igdb.com/igdb/image/upload/t_cover_big/co20r3.jpg"]
+    ] as [String : Any]
         
     let amongUs = [
         "ml_title":"Among Us",
         "title":"Among Us",
         "image":"https://images.igdb.com/igdb/image/upload/t_cover_big/co1uaf.jpg",
         "company":"InnerSloth",
-        "description":"Play online or via local WiFi with 4-10 players as you attempt to prep your spaceship for departure, but beware as one will be an impostor bent on killing everyone!"
-    ]
+        "description":"Play online or via local WiFi with 4-10 players as you attempt to prep your spaceship for departure, but beware as one will be an impostor bent on killing everyone!",
+        "recommendations":["https://images.igdb.com/igdb/image/upload/t_cover_big/co1rrb.jpg","https://images.igdb.com/igdb/image/upload/t_cover_big/vuxcu2rkrbokfnf14yse.jpg","https://images.igdb.com/igdb/image/upload/t_cover_big/co1hku.jpg"]
+    ] as [String : Any]
         
     let mario = [
         "ml_title":"Super Mario 3D",
         "title":"Super Mario 3D World",
         "image":"https://images.igdb.com/igdb/image/upload/t_cover_big/co21vd.jpg",
         "company":"Nintendo",
-        "description":"The cat's out of the bag-Super Mario 3D World is coming to the Nintendo Switch system! Pounce and climb through dozens of colorful stages! Mario (and his friends) can use power-ups like the Super Bell, which grants catlike abilities, like climbing and scratching."
-    ]
+        "description":"The cat's out of the bag-Super Mario 3D World is coming to the Nintendo Switch system! Pounce and climb through dozens of colorful stages! Mario (and his friends) can use power-ups like the Super Bell, which grants catlike abilities, like climbing and scratching.",
+        "recommendations":["https://images.igdb.com/igdb/image/upload/t_cover_big/co1mxf.jpg","https://images.igdb.com/igdb/image/upload/t_cover_big/co213q.jpg","https://images.igdb.com/igdb/image/upload/t_cover_big/co1mxj.jpg"]
+    ] as [String : Any]
     
     
 }
