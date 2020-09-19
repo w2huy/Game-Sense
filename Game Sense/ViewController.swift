7f95851f90e30308f1010ffb3f7f9d9a0cf46792
Huynh, William <huynhwi2@msu.edu>
2020-09-18 22:42:58 -0400

    Initial Commit

---
+++ Game Sense/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Game Sense
+//
+//  Created by William Huynh on 9/18/20.
+//  Copyright © 2020 wi2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
